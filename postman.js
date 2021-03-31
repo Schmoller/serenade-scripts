8a3307b0b17616a981f3b7acea10260fb816228f
Steven Schmoll <steven@accesstel.com.au>
2021-03-31 15:53:04 +1100

    added some extra features for postman

---
+++ postman.js
@@ -0,0 +1,9 @@
+serenade.app("postman").key("send", "enter", ["control"]);
+
+serenade.app("postman").key("go to url", "l", ["control"]);
+
+serenade.app("postman").key("toggle sidebar", "\\", ["control"]);
+serenade.app("postman").key("import", "o", ["control"]);
+serenade.app("postman").key("find", "f", ["control", "shift"]);
+
+serenade.app("postman").key("new", "n", ["control"]);
